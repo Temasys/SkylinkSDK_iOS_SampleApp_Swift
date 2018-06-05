ec423f15a8bc12b285a6ada6c8b40e2258dc29ee
Yuxi <yuxi.liu@temasys.com.sg>
2018-06-05 12:37:49 +0800

    Add the latest Swift 4.2, open with Xcode 10

---
+++ SampleAppSwift4.2_Xcode10/SampleApp/MessagesViewController.swift
@@ -0,0 +1,328 @@
+//
+//  MessagesViewController.swift
+//  SampleApp
+//
+//  Created by Yuxi on 26/7/17.
+//  Copyright © 2017 Yuxi. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class MessagesViewController: UIViewController, SKYLINKConnectionLifeCycleDelegate, SKYLINKConnectionMessagesDelegate, SKYLINKConnectionRemotePeerDelegate, UITextFieldDelegate, UITableViewDataSource, UITableViewDelegate {
+
+    
+    let ROOM_NAME = ROOM_MESSAGES
+    lazy var messages = [[String : Any]]()
+    lazy var peers = [String : Any]()
+    let skylinkApiKey = SKYLINK_APP_KEY
+    let skylinkApiSecret = SKYLINK_SECRET
+    weak var topView: UIView!
+    lazy var skylinkConnection: SKYLINKConnection = {
+        let config = SKYLINKConnectionConfig()
+        config.video = false
+        config.audio = false
+        config.fileTransfer = false
+        config.dataChannel = true // for data chanel messages
+        SKYLINKConnection.setVerbose(true)
+        if let skylinkConnection = SKYLINKConnection(config: config, appKey: skylinkApiKey) {
+            skylinkConnection.lifeCycleDelegate = self
+            skylinkConnection.messagesDelegate = self
+            skylinkConnection.remotePeerDelegate = self
+            return skylinkConnection
+        } else {
+            return SKYLINKConnection()
+        }
+    }()
+    
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var peersButton: UIButton!
+    @IBOutlet weak var nicknameTextField: UITextField!
+    @IBOutlet weak var messageTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var messageTypeSegmentControl: UISegmentedControl!
+    @IBOutlet weak var isPublicSwitch: UISwitch!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setupInfo()
+    }
+    
+    fileprivate func setupUI() {
+        nicknameTextField.delegate = self
+        messageTextField.delegate = self
+        skylinkLog("SKYLINKConnection version = \(SKYLINKConnection.getSkylinkVersion())")
+        title = "Messages"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "Cancel"), style: .plain, target: self, action: #selector(disconnect))
+        let infoButton = UIButton(type: .infoLight)
+        infoButton.addTarget(self, action: #selector(showInfo), for: UIControl.Event.touchUpInside)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: infoButton)
+        updatePeersButtonTitle()
+    }
+    
+    fileprivate func setupInfo() {
+        skylinkConnection.connectToRoom(withSecret: skylinkApiSecret, roomName: ROOM_NAME, userInfo: nil)
+    }
+    
+    @objc fileprivate func disconnect() {
+        skylinkConnection.disconnect { [weak weakSelf = self] in
+            weakSelf?.navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    @objc fileprivate func showInfo() {
+        let title = "Infos"
+        let message = "\nRoom name:\n\(ROOM_NAME)\n\nLocal ID:\n\(skylinkConnection.myPeerId ?? "")\n\nKey: •••••" + (skylinkApiKey as NSString).substring(with: NSRange(location: 0, length: skylinkApiKey.count - 7)) + "\n\nSkylink version \(SKYLINKConnection.getSkylinkVersion())"
+        alertMessage(msg_title: title, msg: message)
+    }
+    
+    fileprivate func alertMessage(msg_title: String, msg:String) {
+        let alertController = UIAlertController(title: msg_title , message: msg, preferredStyle: .alert)
+        let OKAction = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(OKAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    // MARK: SKYLINKConnectionLifeCycleDelegate
+    func connection(_ connection: SKYLINKConnection, didConnectWithMessage errorMessage: String!, success isSuccess: Bool) {
+        if isSuccess {
+            skylinkLog("Inside \(#function)")
+            DispatchQueue.main.async { [weak weakSelf = self] in
+                weakSelf?.messageTextField.isEnabled = true
+                weakSelf?.messageTextField.isHidden = false
+                weakSelf?.nicknameTextField.isEnabled = true
+                weakSelf?.nicknameTextField.isHidden = false
+                weakSelf?.sendButton.isEnabled = true
+                weakSelf?.sendButton.isHidden = false
+                weakSelf?.messageTextField.becomeFirstResponder()
+            }
+        } else {
+            let msgTitle = "Connection failed"
+            let msg = errorMessage
+            alertMessage(msg_title: msgTitle, msg:msg!)
+            disconnect()
+        }
+        activityIndicator.stopAnimating()
+    }
+    
+    func connection(_ connection: SKYLINKConnection, didDisconnectWithMessage errorMessage: String!) {
+        let alert = UIAlertController(title: "Disconnected", message: errorMessage, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        present(alert, animated: true) { [weak weakSelf = self] in
+            weakSelf?.disconnect()
+        }
+    }
+    
+    // MARK: SKYLINKConnectionRemotePeerDelegate
+    func connection(_ connection: SKYLINKConnection, didJoinPeer userInfo: Any!, mediaProperties pmProperties: SKYLINKPeerMediaProperties!, peerId: String!) {
+        if let dict = userInfo as? [String : Any] {
+            var displayNickName = dict["nickname"] as? String
+            if displayNickName == nil {
+                displayNickName = "ID: \(peerId)"
+            }
+            peers[peerId] = displayNickName!
+        } else if let arr = userInfo as? [Any] {
+            print("arr ---> ", arr)
+            peers[peerId] = peerId
+        } else if let str = userInfo as? String {
+            print("str ---> ", str)
+            peers[peerId] = peerId
+        } else {
+            print("Cannot resolve userinfo")
+        }
+        updatePeersButtonTitle()
+    }
+    
+    func connection(_ connection: SKYLINKConnection, didLeavePeerWithMessage errorMessage: String!, peerId: String!) {
+        peers.removeValue(forKey: peerId)
+        updatePeersButtonTitle()
+    }
+    
+    func connection(_ connection: SKYLINKConnection, didReceiveUserInfo userInfo: Any!, peerId: String!) {
+        peers.removeValue(forKey: peerId)
+        if let dict = userInfo as? [String : Any] {
+            var displayNickName = dict["nickname"] as? String
+            if displayNickName == nil {
+                displayNickName = "ID: \(peerId)"
+            }
+            peers[peerId] = displayNickName!
+        } else if let arr = userInfo as? [Any] {
+            print("arr ---> ", arr)
+            peers[peerId] = peerId
+        } else if let str = userInfo as? String {
+            print("str ---> ", str)
+            peers[peerId] = peerId
+        } else {
+            print("Cannot resolve userinfo")
+        }
+        updatePeersButtonTitle()
+        tableView.reloadData()
+    }
+    
+    // MARK: SKYLINKConnectionMessagesDelegate
+    func connection(_ connection: SKYLINKConnection, didReceiveCustomMessage message: Any!, public isPublic: Bool, peerId: String!) {
+        messages.insert(["message" : message, "isPublic" : isPublic, "peerId" : peerId, "type" : "signaling server"], at: 0)
+        tableView.reloadSections(IndexSet(integer: 0), with: .fade)
+    }
+    
+    func connection(_ connection: SKYLINKConnection, didReceiveDCMessage message: Any!, public isPublic: Bool, peerId: String!) {
+        messages.insert(["message" : message, "isPublic" : isPublic, "peerId" : peerId, "type" : "P2P"], at: 0)
+        tableView.reloadSections(IndexSet(integer: 0), with: .fade)
+    }
+    
+    func connection(_ connection: SKYLINKConnection, didReceiveBinaryData data: Data!, peerId: String!) {
+        let maybeString = String(data: data, encoding: .utf8)
+        messages.insert(["message" : maybeString ?? "Binary data of length \(UInt(data.count))", "isPublic" : false, "peerId" : peerId, "type" : "binary data"], at: 0)
+        tableView.reloadSections(IndexSet(integer: 0), with: .fade)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "messageCell", for: indexPath)
+        let aMessage = messages[indexPath.row]
+        cell.textLabel?.text = aMessage["message"] as? String ?? ""
+        let equalStr = skylinkConnection.myPeerId
+        if aMessage["peerId"] as? String == equalStr {
+            cell.backgroundColor = .yellow
+            cell.detailTextLabel?.text = (aMessage["isPublic"] as? Bool ?? false) ? "Sent to all" : "Sent privately"
+        } else {
+            cell.detailTextLabel?.text = "From \(peers[aMessage["peerId"] as? String ?? ""] ?? "") via \(aMessage["type"] as? String ?? "") • \(aMessage["isPublic"] as? Bool ?? false ? "Public" : "Private")"
+            cell.backgroundColor = .white
+        }
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let message = messages[indexPath.row]
+        let messageDetails = String(format:"Message \n %@ \n\n From: \n %@ \n\n %@", message["message"] as? String ?? "", (message["peerId"] as? String ?? "") == (skylinkConnection.myPeerId as String) ? "me" : (message["peerId"] as? String ?? ""), (message["isPublic"] as? Bool ?? false) ? "Public" : "Private")
+        let msgTitle = "Message Details"
+        let msg = messageDetails
+        alertMessage(msg_title: msgTitle, msg: msg)
+    }
+    
+    // MARK: - Utils
+    func processMessage() {
+        if isPublicSwitch.isOn && messageTypeSegmentControl.selectedSegmentIndex == 2 {
+            let msgTitle = "Binary data is private."
+            let msg = "\nTo send your message as binary data, uncheck the \"Public\" UISwitch."
+            alertMessage(msg_title: msgTitle, msg: msg)
+            hideKeyboardIfNeeded()
+        } else if messageTextField.hasText {
+            let message = messageTextField.text
+            if !(isPublicSwitch.isOn) {
+                if peers.count != 0 {
+                    let alert = UIAlertController(title: "Choose a private recipient.", message: "\nYou're about to send a private message\nWho do you want to send it to ?", preferredStyle: .alert)
+                    let noAction = UIAlertAction(title: "Cancel", style: .default)
+                    for peerDicKey in peers.keys {
+                        let yesAction = UIAlertAction(title: peers[peerDicKey] as? String, style: .default) { [weak weakSelf = self] _ in
+                            weakSelf?.alertMessage(msg_title: message ?? "", msg: peerDicKey)
+                        }
+                        alert.addAction(yesAction)
+                    }
+                    alert.addAction(noAction)
+                    present(alert, animated: true, completion: nil)
+                } else {
+                    let msgTitle = "No peer connected."
+                    let msg = "\nYou can't define a private recipient since there is no peer connected."
+                    alertMessage(msg_title: msgTitle, msg: msg)
+                }
+            } else {
+                sendMessage(message: message ?? "", forPeerId: nil)
+            }
+        } else {
+            let msgTitle = "Empty message"
+            let msg = "\nType the message to be sent."
+            alertMessage(msg_title: msgTitle, msg: msg)
+        }
+    }
+    
+    func sendMessage(message: String, forPeerId peerId: String?) {
+        var showSentMessage = true
+        switch messageTypeSegmentControl.selectedSegmentIndex {
+        case 0:
+            skylinkConnection.sendDCMessage(message, peerId: peerId)
+            skylinkLog("Finish DCMessage")
+        case 1:
+            skylinkConnection.sendCustomMessage(message, peerId: peerId)
+        case 2:
+            if peerId != nil {
+                let data = message.data(using: .utf8)
+                if data != nil {
+                    skylinkConnection.sendBinaryData(data, peerId: peerId)
+                } else {
+                    let msgTitle = "Exeption when sending binary data"
+                    let msg = "MCU can be enabled/disabled in Key configuration on the developer portal: http://developer.temasys.com.sg/"
+                    alertMessage(msg_title: msgTitle, msg: msg)
+                    showSentMessage = false
+                }
+            }
+        default:
+            break
+        }
+        if showSentMessage {
+            messageTextField.text = ""
+            messages.insert(["message": message, "isPublic": peerId == nil, "peerId": skylinkConnection.myPeerId], at: 0)
+            tableView.reloadSections(IndexSet(integer: 0), with: .fade)
+        } else {
+            hideKeyboardIfNeeded()
+        }
+    }
+    
+    fileprivate func updatePeersButtonTitle() {
+        let peersCount = peers.count
+        if peersCount == 0 {
+            peersButton.setTitle("No Peer", for: UIControl.State.normal)
+        } else {
+            peersButton.setTitle("\(peersCount) peer" + (peersCount > 1 ? "s " : ""), for: UIControl.State.normal)
+        }
+    }
+    
+    func updateNickname() {
+        if nicknameTextField.hasText {
+            skylinkConnection.sendUserInfo(["nickname" : nicknameTextField.text])
+        } else {
+            let msgTitle = "Empty nickname"
+            let msg = "\nType the nickname to set."
+            alertMessage(msg_title: msgTitle, msg: msg)
+        }
+    }
+    
+    func hideKeyboardIfNeeded() {
+        messageTextField.resignFirstResponder()
+        nicknameTextField.resignFirstResponder()
+    }
+    
+    // MARK: - UITextField delegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == nicknameTextField {
+            updateNickname()
+        } else if textField == messageTextField {
+            processMessage()
+        }
+        hideKeyboardIfNeeded()
+        return true
+    }
+    
+    // MARK: IBFuction
+    @IBAction func sendTap() {
+        processMessage()
+    }
+    
+    @IBAction func diswmissKeyboardTap() {
+        hideKeyboardIfNeeded()
+    }
+    
+    @IBAction func peersTap(sender: UIButton) {
+        let msgTitle = sender.titleLabel?.text
+        let msg = peers.description
+        alertMessage(msg_title: msgTitle ?? "", msg: msg)
+    }
+}
---
+++ SampleAppSwift4.2_Xcode10/SampleApp/VideoCallViewController.swift
@@ -0,0 +1,239 @@
+//
+//  VideoCallViewController.swift
+//  SampleApp
+//
+//  Created by Yuxi on 26/7/17.
+//  Copyright © 2017 Yuxi. All rights reserved.
+//
+
+import UIKit
+
+class VideoCallViewController: UIViewController, SKYLINKConnectionLifeCycleDelegate, SKYLINKConnectionMediaDelegate, SKYLINKConnectionRemotePeerDelegate {
+
+    @IBOutlet weak var localVideoContainerView: UIView!
+    @IBOutlet weak var remotePeerVideoContainerView: UIView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var btnFlipCamera: UIButton!
+    @IBOutlet weak var btnAudioTap: UIButton!
+    @IBOutlet weak var btnVideoTap: UIButton!
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    
+    var peerVideoSize: CGSize!
+    var peerVideoView: UIView!
+    weak var topView: UIView!
+    let ROOM_NAME = ROOM_ONE_TO_ONE_VIDEO
+
+    var remotePeerId: String?
+    let skylinkApiKey = SKYLINK_APP_KEY
+    let skylinkApiSecret = SKYLINK_SECRET
+    lazy var skylinkConnection: SKYLINKConnection = {
+        // Creating configuration
+        let config = SKYLINKConnectionConfig()
+        config.video = true
+        config.audio = true
+        SKYLINKConnection.setVerbose(true)
+        // Creating SKYLINKConnection
+        if let skylinkConnection = SKYLINKConnection(config: config, appKey: skylinkApiKey) {
+            skylinkConnection.lifeCycleDelegate = self
+            skylinkConnection.mediaDelegate = self
+            skylinkConnection.remotePeerDelegate = self
+            return skylinkConnection
+        } else {
+            return SKYLINKConnection()
+        }
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        setupInfo()
+        UIApplication.shared.isIdleTimerDisabled = true
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        if peerVideoView != nil {
+            peerVideoView.frame = aspectFillRectForSize(insideSize: peerVideoSize, containedInRect: remotePeerVideoContainerView.frame)
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        UIApplication.shared.isIdleTimerDisabled = false
+    }
+    
+    fileprivate func setupUI() {
+        skylinkLog("SKYLINKConnection version = \(SKYLINKConnection.getSkylinkVersion())")
+        title = "1-1 Video Call"
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "Cancel"), style: .plain, target: self, action: #selector(disconnect))
+        let infoButton = UIButton(type: .infoLight)
+        infoButton.addTarget(self, action: #selector(showInfo), for: UIControl.Event.touchUpInside)
+        navigationItem.rightBarButtonItem = UIBarButtonItem(customView: infoButton)
+        //Disable Btn
+        btnAudioTap.isEnabled = false
+        refreshButton.isEnabled = false
+        btnVideoTap.isEnabled = false
+        btnFlipCamera.isEnabled = false
+    }
+    
+    fileprivate func setupInfo() {
+        // Connecting to a room
+        DispatchQueue.global().async { [weak weakSelf = self] in
+            weakSelf?.skylinkConnection.connectToRoom(withSecret: weakSelf?.skylinkApiSecret, roomName: weakSelf?.ROOM_NAME, userInfo: nil)
+        }
+    }
+
+    @objc fileprivate func disconnect() {
+        activityIndicator.startAnimating()
+        skylinkConnection.unlockTheRoom()
+        skylinkConnection.disconnect { [weak weakSelf = self] in
+            weakSelf?.navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    @objc fileprivate func showInfo() {
+        let title = "Infos"
+        let message = "\nRoom name:\n\(ROOM_NAME)\n\nLocal ID:\n\(skylinkConnection.myPeerId ?? "")\n\nKey: •••••" + (skylinkApiKey as NSString).substring(with: NSRange(location: 0, length: skylinkApiKey.count - 7)) + "\n\nSkylink version \(SKYLINKConnection.getSkylinkVersion())"
+        alertMessage(msg_title: title, msg: message)
+    }
+    
+    fileprivate func alertMessage(msg_title: String, msg:String) {
+        let alertController = UIAlertController(title: msg_title , message: msg, preferredStyle: .alert)
+        let OKAction = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(OKAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    // MARK: - SKYLINKConnectionLifeCycleDelegate
+    func connection(_ connection: SKYLINKConnection, didConnectWithMessage errorMessage: String!, success isSuccess: Bool) {
+        if isSuccess {
+            skylinkLog("Inside \(#function)")
+        } else {
+            let msgTitle = "Connection failed"
+            let msg = errorMessage
+            alertMessage(msg_title: msgTitle, msg:msg!)
+            navigationController?.popViewController(animated: true)
+        }
+        DispatchQueue.main.async { [weak weakSelf = self] in
+            //Enable Btn
+            weakSelf?.btnAudioTap.isEnabled = true
+            weakSelf?.btnVideoTap.isEnabled = true
+            weakSelf?.refreshButton.isEnabled = true
+            weakSelf?.btnFlipCamera.isEnabled = true
+        }
+        activityIndicator.stopAnimating()
+    }
+    
+    func connection(_ connection: SKYLINKConnection, didDisconnectWithMessage errorMessage: String!) {
+        let alert = UIAlertController(title: "Disconnected", message: errorMessage, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        present(alert, animated: true) { [weak weakSelf = self] in
+            weakSelf?.disconnect()
+        }
+    }
+    
+    func connection(_ connection: SKYLINKConnection, didRenderUserVideo userVideoView: UIView!) {
+        addRenderedVideo(videoView: userVideoView, insideContainer: localVideoContainerView, mirror: true)
+    }
+    
+    func connection(_ connection: SKYLINKConnection, didJoinPeer userInfo: Any!, mediaProperties pmProperties: SKYLINKPeerMediaProperties!, peerId: String!) {
+        activityIndicator.stopAnimating()
+        remotePeerId = peerId
+    }
+    
+    func connection(_ connection: SKYLINKConnection, didRenderPeerVideo peerVideoView: UIView!, peerId: String!) {
+        addRenderedVideo(videoView: peerVideoView, insideContainer: remotePeerVideoContainerView, mirror: false)
+    }
+    
+    func connection(_ connection: SKYLINKConnection, didLeavePeerWithMessage errorMessage: String!, peerId: String!) {
+        skylinkConnection.unlockTheRoom()
+        alertMessage(msg_title: "Peer Left", msg: "\nPeer ID:\(peerId)\n has been left")
+    }
+    
+    // MARK: - SKYLINKConnectionMediaDelegate
+    func connection(_ connection: SKYLINKConnection, didChangeVideoSize videoSize: CGSize, videoView: UIView!) {
+        if videoSize.height > 0 && videoSize.width > 0 {
+            var correspondingContainerView: UIView
+            if videoView.isDescendant(of: localVideoContainerView) {
+                correspondingContainerView = localVideoContainerView
+            } else {
+                correspondingContainerView = remotePeerVideoContainerView
+                peerVideoView = videoView
+                peerVideoSize = videoSize
+            }
+            videoView.frame = aspectFillRectForSize(insideSize: videoSize, containedInRect: correspondingContainerView.frame)
+            // for aspect fit, use AVMakeRectWithAspectRatioInsideRect(videoSize, correspondingContainerView.bounds);
+        }
+    }
+    
+    // MARK: - Other
+    
+    // for didRender.. Delegates
+    func addRenderedVideo(videoView: UIView, insideContainer containerView: UIView, mirror shouldMirror: Bool) {
+        videoView.frame = containerView.bounds
+        _ = containerView.subviews.map { $0.removeFromSuperview() }
+        containerView.insertSubview(videoView, at: 0)
+    }
+    
+    func aspectFillRectForSize(insideSize: CGSize, containedInRect containerRect: CGRect) -> CGRect {
+        var maxFloat: CGFloat = 0
+        if containerRect.size.height > containerRect.size.width {
+            maxFloat = containerRect.size.height
+        } else if containerRect.size.height < containerRect.size.width {
+            maxFloat = containerRect.size.width
+        } else {
+            maxFloat = 0
+        }
+        var aspectRatio: CGFloat = 0
+        if insideSize.height != 0 {
+            aspectRatio = insideSize.width / insideSize.height
+        } else {
+            aspectRatio = 1
+        }
+        var frame = CGRect(x: 0, y: 0, width: containerRect.size.width, height: containerRect.size.height)
+        if insideSize.width < insideSize.height {
+            frame.size.width = maxFloat
+            frame.size.height = frame.size.width / aspectRatio
+        } else {
+            frame.size.height = maxFloat;
+            frame.size.width = frame.size.height * aspectRatio
+        }
+        frame.origin.x = (containerRect.size.width - frame.size.width) / 2
+        frame.origin.y = (containerRect.size.height - frame.size.height) / 2
+        return frame
+    }
+    
+    // MARK: IBAction TouchUp
+    @IBAction func toogleVideoTap(sender: AnyObject) {
+        skylinkConnection.muteVideo(!skylinkConnection.isVideoMuted())
+        sender.setImage(UIImage(named: ((skylinkConnection.isVideoMuted()) ? "NoVideoFilled.png" : "VideoCall.png")), for: UIControl.State.normal)
+        localVideoContainerView.isHidden = skylinkConnection.isVideoMuted()
+    }
+    
+    @IBAction func toogleSoundTap(sender: AnyObject) {
+        skylinkConnection.muteAudio(!skylinkConnection.isAudioMuted())
+        sender.setImage(UIImage(named: ((skylinkConnection.isAudioMuted()) ? "NoMicrophoneFilled.png" : "Microphone.png")), for: UIControl.State.normal)
+    }
+    
+    @IBAction func switchCameraTap() {
+        skylinkConnection.switchCamera()
+    }
+    
+    @IBAction func refreshTap() {
+        if (remotePeerId != nil) {
+            activityIndicator.startAnimating()
+            skylinkConnection.unlockTheRoom()
+            skylinkConnection.refreshConnection(remotePeerId!)
+        } else {
+            let msgTitle = "No peer connexion to refresh"
+            let msg = "Tap this button to refresh the peer connexion if needed."
+            alertMessage(msg_title: msgTitle, msg:msg)
+        }
+    }
+    
+    func connection(_ connection: SKYLINKConnection, didGetWebRTCStats stats: [AnyHashable : Any]!, forPeerId peerId: String!, mediaDirection: Int32) {
+        skylinkLog("#Stats\nmd=\(mediaDirection) pid=\(peerId)\n\(stats.description)")
+    }
+}
