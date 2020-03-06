41c2d63d240659986c1863dc1a2c5041102fba21
Charlie <hungcuong.hoang@temasys.com.sg>
2020-03-06 13:15:22 +0800

    rename default room names

--- SampleAppSwift5/SampleApp_Swift/Constants.swift
+++ SampleAppSwift5/SampleApp_Swift/Constants.swift
@@ -27,12 +27,12 @@ var APP_KEYS: [String: String] = ["APP_KEY1": "APP_SECRET1",
                                   "APP_KEY2": "APP_SECRET2"]
 
 var ROOM_NAME = ""
-var ROOM_ONE_TO_ONE_VIDEO = "ROOM_ONE_TO_ONE_VIDEO"
-var ROOM_MULTI_VIDEO = "ROOM_MULTI_VIDEO"
-var ROOM_AUDIO = "ROOM_AUDIO"
-var ROOM_MESSAGES = "ROOM_MESSAGES"
-var ROOM_FILE_TRANSFER = "ROOM_FILE_TRANSFER"
-var ROOM_DATA_TRANSFER = "ROOM_DATA_TRANSFER"
+var ROOM_ONE_TO_ONE_VIDEO = "One to one Media"
+var ROOM_MULTI_VIDEO = "Multiparty"
+var ROOM_AUDIO = "Audio"
+var ROOM_MESSAGES = "Chat"
+var ROOM_FILE_TRANSFER = "File Transfer"
+var ROOM_DATA_TRANSFER = "Data Streaming"
 var appFilesFolder = ""
 
 var USER_NAME = UIDevice.current.name
